43429c8a4ac4108c64b51fac7d3cc69a6a64d2ec
Demarco Cristian <demarcocristian07@gmail.com>
2024-07-07 14:14:52 +0200

    l18n and l10n: Internationalization and Localization

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -8,6 +8,10 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_mail import Mail
 from flask_moment import Moment
+from flask_babel import Babel
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -48,6 +52,7 @@ if not app.debug:
 
 mail = Mail(app)
 moment = Moment(app)
+babel = Babel(app, locale_selector=get_locale)
 
 
 from app import routes, models, errors
